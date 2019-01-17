29b9d8a08fb872941feab154d6a17ba2164ea027
Dak Prescott <prescott@PeDePano.braghim>
2019-01-17 10:19:52 -0200

    Imagem online com placeholder

--- lib/layout.dart
+++ lib/layout.dart
@@ -12,7 +12,16 @@ class Layout {
         child: ListView(
           children: <Widget>[
             ListTile(
-              leading: Icon(Icons.arrow_right),
+              // leading: Icon(Icons.arrow_right),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: FadeInImage.assetNetwork(
+                  placeholder: 'assets/images/ellipsis-64.gif',
+                  image: 'https://i.ibb.co/mFNcFMw/iconfinder-go-home-118770.png',
+                  width: 32,
+                  height: 32,
+                ),
+              ),
               title: Text('Home'),
               onTap: () {
                 Navigator.of(context).pushNamed(HomePage.tag);
